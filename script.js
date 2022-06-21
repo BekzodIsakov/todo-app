ff421f27792e1071a53c9ae13f3abb25fe3517b4
Bekzod <bekzodisakov18@gmail.com>
2022-06-21 21:00:46 +0500

    edit task functionality

--- script.js
+++ script.js
@@ -1,12 +1,84 @@
-const newTask = document.getElementById("new-task-input");
 const taskTemplate = document.getElementById("task-template");
+const editInputTemplate = document.getElementById("edit-input-template");
+const newTaskInput = document.getElementById("new-task-input");
 const addTaskBtn = document.getElementById("add-task");
-let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
-const deleteAllBtn = document.getElementById('delete-all-btn');
-const clearAllModalBbtn = document.getElementById('clearAll-Modal-btn');
+const confirmDeleteAllBtn = document.getElementById("confirm-delete-all-btn");
+const deleteAllBtn = document.getElementById("clearAll-Modal-btn");
 
-newTask.addEventListener("keypress", (event) => {
-  if (!newTask.value) {
+let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
+
+const renderTasks = (editedTaskId) => {
+  tasks.length > 1
+    ? deleteAllBtn.classList.remove("d-none")
+    : deleteAllBtn.classList.add("d-none");
+
+  const tasksList = document.getElementById("tasks-list");
+  tasksList.innerHTML = null;
+
+  tasks.forEach((task, index) => {
+    const taskTmp = document.importNode(taskTemplate.content, true);
+    const taskText = taskTmp.getElementById("task-content");
+    const completeBtn = taskTmp.getElementById("complete-task-btn");
+    const editBtn = taskTmp.getElementById("edit-task-btn");
+    const deleteBtn = taskTmp.getElementById("delete-task-btn");
+    const checkedIcon = completeBtn.firstElementChild;
+
+    taskText.innerText = task.text;
+
+    completeBtn.onclick = () => toggleTaskCompletion(task.id);
+    editBtn.onclick = () => editTask(task.id);
+    deleteBtn.onclick = () => deleteTask(task.id);
+
+    if (task.isCompleted) {
+      taskText.classList.add("text-decoration-line-through", "text-black-50");
+      checkedIcon.classList.remove("bi-check-circle");
+      checkedIcon.classList.add("bi-check-circle-fill");
+    }
+
+    const inputTmp = document.importNode(editInputTemplate.content, true);
+    const input = inputTmp.getElementById("edit-task-input");
+    const confirmEditBtn = inputTmp.getElementById("confirm-edit-btn");
+
+    if (editedTaskId === task.id) {
+      input.value = task.text;
+      tasksList.appendChild(inputTmp);
+      confirmEditBtn.onclick = () => confirmTaskEdit(index);
+    } else {
+      tasksList.appendChild(taskTmp);
+    }
+  });
+};
+
+const confirmTaskEdit = (taskIndex) => {
+  const editTaskInput = document.getElementById("edit-task-input");
+
+  if (tasks[taskIndex].text !== editTaskInput.value) {
+    tasks[taskIndex] = {...tasks[taskIndex], text: editTaskInput.value};
+    saveTasks();
+  }
+
+  renderTasks();
+};
+
+function render() {
+  renderTasks();
+}
+
+const addTask = () => {
+  const { value } = newTaskInput;
+
+  if (!value) {
+    return;
+  }
+
+  tasks.push({ id: Date.now().toString(), text: value, isCompleted: false });
+  localStorage.setItem("tasks", JSON.stringify(tasks));
+  document.getElementById("new-task-input").value = "";
+  renderTasks();
+};
+
+newTaskInput.addEventListener("keypress", (event) => {
+  if (!newTaskInput.value) {
     return;
   }
 
@@ -17,6 +89,10 @@ newTask.addEventListener("keypress", (event) => {
 
 const saveTasks = () => {
   localStorage.setItem("tasks", JSON.stringify(tasks));
+};
+
+const editTask = (taskId) => {
+  renderTasks(taskId);
 };
 
 const deleteTask = (taskId) => {
@@ -36,58 +112,14 @@ const toggleTaskCompletion = (taskId) => {
   renderTasks();
 };
 
-const addTask = () => {
-  const { value } = newTask;
-
-  if (!value) {
-    return;
-  }
-
-  tasks.push({ id: Date.now().toString(), text: value, isCompleted: false });
-  localStorage.setItem("tasks", JSON.stringify(tasks));
-  document.getElementById("new-task-input").value = "";
-  renderTasks();
-};
-
 addTaskBtn.addEventListener("click", addTask);
-
-const renderTasks = () => {
-  tasks.length > 1 ? clearAllModalBbtn.classList.remove('d-none') : clearAllModalBbtn.classList.add('d-none');
-
-  const tasksList = document.getElementById("tasks-list");
-  tasksList.innerHTML = null;
-
-  tasks.forEach((task) => {
-    const taskTmp = document.importNode(taskTemplate.content, true);
-    const checkedIcon = taskTmp.getElementById("checked-icon");
-    const taskText = taskTmp.getElementById("task-content");
-    taskText.innerText = task.text;
-
-    if (task.isCompleted) {
-      taskText.classList.add("text-decoration-line-through", "text-black-50");
-      checkedIcon.classList.remove("bi-check-circle");
-      checkedIcon.classList.add("bi-check-circle-fill");
-    }
-
-    checkedIcon.parentNode.onclick = () => toggleTaskCompletion(task.id);
-
-    taskTmp
-      .getElementById("delete-task-btn")
-      .addEventListener("click", () => deleteTask(task.id));
-    tasksList.appendChild(taskTmp);
-  });
-};
-
-function render() {
-  renderTasks();
-}
 
 const clearAllTasks = () => {
   tasks = [];
   saveTasks();
   renderTasks();
-}
+};
 
-deleteAllBtn.onclick = clearAllTasks;
+confirmDeleteAllBtn.onclick = clearAllTasks;
 
 render();
